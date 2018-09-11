8277c75cb47b5c5cf6db716f0cc510c4caa8476a
Vish Vishvanath <vishvish@users.noreply.github.com>
2018-09-11 23:00:02 +0100

    Rename package

--- src/main/java/com/vishvish/example/springconsulexample/Application.java
+++ src/main/java/com/vishvish/example/springconsulexample/Application.java
@@ -1,4 +1,4 @@
-package com.nllservices.example.springconsulexample;
+package com.vishvish.example.springconsulexample;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
--- src/test/java/com/vishvish/example/springconsulexample/SpringConsulExampleApplicationTests.java
+++ src/test/java/com/vishvish/example/springconsulexample/SpringConsulExampleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nllservices.example.springconsulexample;
+package com.vishvish.example.springconsulexample;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
